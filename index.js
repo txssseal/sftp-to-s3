25d4f13041cb2a3ae38772acefc47963eaf36eef
Hawley Brett <annehawleybrett@gmail.com>
2019-02-28 16:40:55 -0500

    linting

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const Client = require('ssh2-sftp-client');
-const streamToString = require('./lib/streamToString');
-const retrieveFileStreams = require('./lib/retrieveFileStreams');
-const uploadToS3 = require('./lib/uploadToS3');
-const path = require('path');
+const Client = require("ssh2-sftp-client");
+const streamToString = require("./lib/streamToString");
+const retrieveFileStreams = require("./lib/retrieveFileStreams");
+const uploadToS3 = require("./lib/uploadToS3");
+const path = require("path");
 
 const SftpToS3 = {
   batch: function(config) {
-    const sftp = new Client()
+    const sftp = new Client();
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       // handle path errors
       if (config.aws === undefined) {
         console.error("aws key is require for config");
@@ -34,40 +34,42 @@ const SftpToS3 = {
         return;
       }
 
-      return sftp.connect(config.sftp)
+      return sftp
+        .connect(config.sftp)
         .then(() => {
           return sftp.list(config.fileDownloadDir);
         })
-        .then((fileList) => {
-          return retrieveFileStreams(sftp, config, fileList, "sftp")
+        .then(fileList => {
+          return retrieveFileStreams(sftp, config, fileList, "sftp");
         })
-        .then((fileStreams) => {
-          return streamToString(fileStreams)
+        .then(fileStreams => {
+          return streamToString(fileStreams);
         })
-        .then((dataArray) => {
-          return uploadToS3.putBatch(config, dataArray)
+        .then(dataArray => {
+          return uploadToS3.putBatch(config, dataArray);
         })
-        .then((files) => {
-          sftp.mkdir(config.completedDir, true)
+        .then(files => {
+          sftp.mkdir(config.completedDir, true);
           return sftp.list(config.fileDownloadDir);
         })
-        .then((files) => {
-          files.map((file) => {
-            sftp.rename(config.fileDownloadDir + file.name, config.completedDir + file.name);
-          })
-          console.log("upload finished")
-          sftp.end()
-          return resolve("ftp files uploaded")
+        .then(files => {
+          files.map(file => {
+            sftp.rename(
+              config.fileDownloadDir + file.name,
+              config.completedDir + file.name
+            );
+          });
+          console.log("upload finished");
+          sftp.end();
+          return resolve("ftp files uploaded");
         })
-        .catch( function(err) {
+        .catch(function(err) {
           console.error("Error", err);
-          sftp.end()
-          return reject(err)
-        })
-    })
+          sftp.end();
+          return reject(err);
+        });
+    });
   }
-}
+};
 
-module.exports = SftpToS3
-
-
+module.exports = SftpToS3;
